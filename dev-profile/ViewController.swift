899741386aa02ec60cd116a19a64a6989c4724d9
K.K <koceto1973@yahoo.ca>
2018-09-11 10:40:55 +0300

    Stage 1
    iphone view ready

--- dev-profile/ViewController.swift
+++ dev-profile/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DevOpsLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        DevOpsLogo.clipsToBounds = true
+        DevOpsLogo.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
